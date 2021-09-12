8c75d62adb56437957055dc9496e88c67c3b8a9b
JamBab2100 <90546586+JamBab2100@users.noreply.github.com>
2021-09-12 12:54:14 +0530

    Create Component.php

---
+++ Component.php
@@ -0,0 +1,21 @@
+<?php
+class Component {
+//default length
+public $length = array("width"=>"100%","height"=>"800px");
+//default colors
+public $color = array("bg"=>"","text"=>"","border"=> "");
+
+
+
+function __construct($id = '') {
+
+
+}
+
+
+
+
+
+}
+
+?>
