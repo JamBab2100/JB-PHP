d66844373fc95ef4219e72e111618576623bbed5
JamBab2100 <90546586+JamBab2100@users.noreply.github.com>
2021-09-12 14:05:32 +0530

    Update Component.php

--- Component.php
+++ Component.php
@@ -1,18 +1,50 @@
 <?php
 class Component {
+
+private $Ename ;
 //default length
 public $length = array("width"=>"100%","height"=>"800px");
 //default colors
-public $color = array("bg"=>"","text"=>"","border"=> "");
+public $color = array("bg"=>"","text"=>"");
+//default text size
+public $text_size = '18px';
+//default hint 
+public $hint = 'Enter here';
+//default type
+public $type = 'Textbox';
+public $input_type = 'text';
+
+
+function __construct($id = 'def1') {
+
+$this->Ename = $id . "Element";
+}
+
+public function create(){
+if($type = "Textbox" || $type = "TextBox"){
+
+}
+
+elseif($type = "Combobox" || $type = "ComboBox"){
 
 
 
-function __construct($id = '') {
+}
+
+elseif($type = "Textarea" || $type = "TextArea"){
+
 
 
 }
 
 
+}
+
+
+
+
+
+}
 
 
 
